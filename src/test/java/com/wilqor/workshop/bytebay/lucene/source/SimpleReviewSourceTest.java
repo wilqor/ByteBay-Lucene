651f508ee040bded0287c13c186c30515f907480
wilqor <wilqor@gmail.com>
2018-03-01 21:06:39 +0100

    Moving sources to src/main/java packages

--- src/test/java/com/wilqor/workshop/bytebay/lucene/source/SimpleReviewSourceTest.java
+++ src/test/java/com/wilqor/workshop/bytebay/lucene/source/SimpleReviewSourceTest.java
@@ -1,7 +1,5 @@
-package com.wilqor.workshop.bytebay.lucene;
+package com.wilqor.workshop.bytebay.lucene.source;
 
-import com.wilqor.workshop.bytebay.lucene.source.SimpleReview;
-import com.wilqor.workshop.bytebay.lucene.source.Source;
 import org.junit.Test;
 
 import java.util.List;
@@ -10,10 +8,10 @@ import java.util.stream.Collectors;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
 
-public class CommentedReviewSourceTest {
+public class SimpleReviewSourceTest {
     @Test
     public void shouldParseExample() {
-        List<SimpleReview> reviews = Source.COMMENTED_MODEL.stream()
+        List<SimpleReview> reviews = Source.SIMPLE_MODEL.stream()
                 .collect(Collectors.toList());
 
         assertThat(reviews, hasSize(5));
