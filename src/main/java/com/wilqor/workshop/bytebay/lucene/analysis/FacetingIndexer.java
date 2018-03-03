c7cac3baef0b379b34bc705db1fb6a4a74ca67ed
wilqor <wilqor@gmail.com>
2018-03-03 18:51:48 +0100

    Implement Facets example

---
+++ src/main/java/com/wilqor/workshop/bytebay/lucene/analysis/FacetingIndexer.java
@@ -0,0 +1,89 @@
+package com.wilqor.workshop.bytebay.lucene.analysis;
+
+import com.wilqor.workshop.bytebay.lucene.config.ConfigLoader;
+import com.wilqor.workshop.bytebay.lucene.config.IndexType;
+import com.wilqor.workshop.bytebay.lucene.source.SimpleReview;
+import com.wilqor.workshop.bytebay.lucene.source.Source;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.KeywordAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.facet.FacetField;
+import org.apache.lucene.facet.FacetsConfig;
+import org.apache.lucene.facet.taxonomy.TaxonomyWriter;
+import org.apache.lucene.facet.taxonomy.directory.DirectoryTaxonomyWriter;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.IOUtils;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class FacetingIndexer implements Indexer<SimpleReview> {
+    static final FacetsConfig FACETS_CONFIG = new FacetsConfig();
+    static final String INDEX_SUBDIRECTORY_NAME = "index";
+    static final String TAXONOMY_SUBDIRECTORY_NAME = "taxonomy";
+
+    static final String USER_NAME_FACET_FIELD = "USER_NAME";
+    static final String THUMB_FACET_FIELD = "THUMB";
+    static final String ARTICLE_FACET_FIELD = "ARTICLE_NAME";
+
+    private static final Logger LOGGER = LogManager.getLogger(FacetingIndexer.class);
+
+    private final FSDirectory directory;
+    private final FSDirectory taxonomyDirectory;
+    private final IndexWriter indexWriter;
+    private final TaxonomyWriter taxonomyWriter;
+    private final Analyzer analyzer;
+
+    FacetingIndexer(Path targetDirectory) throws IOException {
+        directory = FSDirectory.open(targetDirectory.resolve(INDEX_SUBDIRECTORY_NAME));
+        LOGGER.info("Opened index in directory: {}", directory.getDirectory());
+        taxonomyDirectory = FSDirectory.open(targetDirectory.resolve(TAXONOMY_SUBDIRECTORY_NAME));
+        taxonomyWriter = new DirectoryTaxonomyWriter(taxonomyDirectory);
+        LOGGER.info("Opened taxonomy in directory: {}", taxonomyDirectory.getDirectory());
+        analyzer = new KeywordAnalyzer();
+        indexWriter = new IndexWriter(directory, new IndexWriterConfig(analyzer));
+    }
+
+    @Override
+    public void index(Source<SimpleReview> source) {
+        source.stream().forEach(simpleReview -> {
+            Document reviewDocument = mapToDocument(simpleReview);
+            try {
+                Document facetedDocument = FACETS_CONFIG.build(taxonomyWriter, reviewDocument);
+                indexWriter.addDocument(facetedDocument);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private Document mapToDocument(SimpleReview simpleReview) {
+        Document reviewDocument = new Document();
+        reviewDocument.add(new FacetField(USER_NAME_FACET_FIELD, simpleReview.getUserName()));
+        reviewDocument.add(new FacetField(THUMB_FACET_FIELD, simpleReview.getThumb().name()));
+        reviewDocument.add(new StringField(ARTICLE_FACET_FIELD, simpleReview.getArticleName(), Field.Store.YES));
+        reviewDocument.add(new FacetField(ARTICLE_FACET_FIELD, simpleReview.getArticleName()));
+        return reviewDocument;
+    }
+
+    @Override
+    public void close() throws Exception {
+        Path directoryPath = directory.getDirectory();
+        Path taxonomyPath = taxonomyDirectory.getDirectory();
+        IOUtils.close(indexWriter, taxonomyWriter, directory, taxonomyDirectory, analyzer);
+        LOGGER.info("Closed index in directory: {}", directoryPath);
+        LOGGER.info("Closed taxonomy in directory: {}", taxonomyPath);
+    }
+
+    public static void main(String[] args) throws Exception {
+        try (Indexer<SimpleReview> indexer = new FacetingIndexer(ConfigLoader.LOADER.getPathForIndex(IndexType.FACETING_EXAMPLE))) {
+            indexer.index(Source.SIMPLE_MODEL);
+        }
+    }
+}
---
+++ src/test/java/com/wilqor/workshop/bytebay/lucene/analysis/FacetingIndexerTest.java
@@ -0,0 +1,98 @@
+package com.wilqor.workshop.bytebay.lucene.analysis;
+
+import com.wilqor.workshop.bytebay.lucene.FSDirectoryReadingTest;
+import com.wilqor.workshop.bytebay.lucene.config.ConfigLoader;
+import com.wilqor.workshop.bytebay.lucene.config.IndexType;
+import com.wilqor.workshop.bytebay.lucene.source.Thumb;
+import org.apache.lucene.facet.FacetResult;
+import org.apache.lucene.facet.Facets;
+import org.apache.lucene.facet.FacetsCollector;
+import org.apache.lucene.facet.LabelAndValue;
+import org.apache.lucene.facet.taxonomy.FastTaxonomyFacetCounts;
+import org.apache.lucene.facet.taxonomy.TaxonomyReader;
+import org.apache.lucene.facet.taxonomy.directory.DirectoryTaxonomyReader;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.IOUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static org.hamcrest.Matchers.arrayWithSize;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class FacetingIndexerTest extends FSDirectoryReadingTest {
+    private static final int TOP_N_LIMIT = 3;
+    private Path directoryPath;
+    private Directory taxonomyDirectory;
+    private TaxonomyReader taxonomyReader;
+
+    @Override
+    protected Path provideDirectoryPath() {
+        return directoryPath.resolve(FacetingIndexer.INDEX_SUBDIRECTORY_NAME);
+    }
+
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        directoryPath = ConfigLoader.LOADER.getPathForIndex(IndexType.FACETING_EXAMPLE);
+        super.setUp();
+        taxonomyDirectory = FSDirectory.open(directoryPath.resolve(FacetingIndexer.TAXONOMY_SUBDIRECTORY_NAME));
+        taxonomyReader = new DirectoryTaxonomyReader(taxonomyDirectory);
+    }
+
+    @Override
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+        IOUtils.close(taxonomyDirectory, taxonomyReader);
+    }
+
+    @Test
+    public void shouldFindMostCommentedArticle() throws Exception {
+        FacetResult topArticlesResult = getFacetResult(new MatchAllDocsQuery(), FacetingIndexer.ARTICLE_FACET_FIELD);
+
+        LabelAndValue[] labelValues = topArticlesResult.labelValues;
+        LabelAndValue topArticle = labelValues[0];
+        assertThat(topArticle.label, is("Lucene best practices"));
+        assertThat(topArticle.value, is(2));
+    }
+
+    @Test
+    public void shouldFindMostActiveUser() throws Exception {
+        FacetResult topUsersResult = getFacetResult(new MatchAllDocsQuery(), FacetingIndexer.USER_NAME_FACET_FIELD);
+
+        LabelAndValue[] labelValues = topUsersResult.labelValues;
+        LabelAndValue topUser = labelValues[0];
+        assertThat(topUser.label, is("zbyszkop"));
+        assertThat(topUser.value, is(3));
+    }
+
+    @Test
+    public void shouldCountThumbsForArticle() throws Exception {
+        TermQuery articleFilterQuery = new TermQuery(new Term(FacetingIndexer.ARTICLE_FACET_FIELD, "Lucene best practices"));
+        FacetResult topThumbsResult = getFacetResult(articleFilterQuery, FacetingIndexer.THUMB_FACET_FIELD);
+
+        LabelAndValue[] labelValues = topThumbsResult.labelValues;
+        assertThat(labelValues, is(arrayWithSize(1)));
+
+        LabelAndValue topThumbs = labelValues[0];
+        assertThat(topThumbs.label, is(Thumb.UP.name()));
+        assertThat(topThumbs.value, is(2));
+    }
+
+    private FacetResult getFacetResult(Query query, String facetFieldName) throws IOException {
+        FacetsCollector facetsCollector = new FacetsCollector();
+        FacetsCollector.search(searcher, query, 0, facetsCollector);
+        Facets facets = new FastTaxonomyFacetCounts(taxonomyReader, FacetingIndexer.FACETS_CONFIG, facetsCollector);
+        return facets.getTopChildren(TOP_N_LIMIT, facetFieldName);
+    }
+}
