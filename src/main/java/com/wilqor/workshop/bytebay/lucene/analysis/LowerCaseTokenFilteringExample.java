d0abeee310a946ff1d4a2a70d581075259399acf
wilqor <wilqor@gmail.com>
2018-03-04 20:51:02 +0100

    Implement LowerCaseTokenFilter example

---
+++ src/main/java/com/wilqor/workshop/bytebay/lucene/analysis/LowerCaseTokenFilteringExample.java
@@ -0,0 +1,32 @@
+package com.wilqor.workshop.bytebay.lucene.analysis;
+
+import com.wilqor.workshop.bytebay.lucene.config.ConfigLoader;
+import com.wilqor.workshop.bytebay.lucene.config.IndexType;
+import com.wilqor.workshop.bytebay.lucene.source.CommentedReview;
+import com.wilqor.workshop.bytebay.lucene.source.Source;
+import com.wilqor.workshop.bytebay.lucene.utils.ThrowingSupplier;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.LowerCaseFilter;
+import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.core.WhitespaceTokenizer;
+
+import java.nio.file.Path;
+
+public class LowerCaseTokenFilteringExample {
+    public static class LowerCaseWhitespaceAnalyzer extends Analyzer {
+        @Override
+        protected TokenStreamComponents createComponents(String fieldName) {
+            Tokenizer tokenizer = new WhitespaceTokenizer();
+            TokenFilter tokenFilter = new LowerCaseFilter(tokenizer);
+            return new TokenStreamComponents(tokenizer, tokenFilter);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Path pathForIndex = ConfigLoader.LOADER.getPathForIndex(IndexType.LOWER_CASE_TOKEN_FILTER_EXAMPLE);
+        ThrowingSupplier<Indexer<CommentedReview>> supplier = () -> new CommentedReviewIndexer(pathForIndex,
+                new LowerCaseWhitespaceAnalyzer());
+        IndexerRunner.of(pathForIndex, supplier, Source.COMMENTED_MODEL).runIndexer();
+    }
+}
---
+++ src/test/java/com/wilqor/workshop/bytebay/lucene/analysis/LowerCaseTokenFilteringTest.java
@@ -0,0 +1,48 @@
+package com.wilqor.workshop.bytebay.lucene.analysis;
+
+import com.wilqor.workshop.bytebay.lucene.FSDirectoryReadingTest;
+import com.wilqor.workshop.bytebay.lucene.config.ConfigLoader;
+import com.wilqor.workshop.bytebay.lucene.config.IndexType;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.TopDocs;
+import org.junit.Test;
+
+import java.nio.file.Path;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class LowerCaseTokenFilteringTest extends FSDirectoryReadingTest {
+    private static final int QUERY_MATCHES_LIMIT = 3;
+
+    @Override
+    protected Path provideDirectoryPath() {
+        return ConfigLoader.LOADER.getPathForIndex(IndexType.LOWER_CASE_TOKEN_FILTER_EXAMPLE);
+    }
+
+    @Test
+    public void shouldRetrieveZeroReviewsForFullArticleName() throws Exception {
+        Query query = new TermQuery(new Term(CommentedReviewIndexer.ARTICLE_NAME_FIELD, "Lucene 101"));
+        TopDocs topDocs = searcher.search(query, QUERY_MATCHES_LIMIT);
+
+        assertThat(topDocs.totalHits, is(0L));
+    }
+
+    @Test
+    public void shouldRetrieveZeroReviewsForArticleNameTermWithInUpperCase() throws Exception {
+        Query query = new TermQuery(new Term(CommentedReviewIndexer.ARTICLE_NAME_FIELD, "Lucene"));
+        TopDocs topDocs = searcher.search(query, QUERY_MATCHES_LIMIT);
+
+        assertThat(topDocs.totalHits, is(0L));
+    }
+
+    @Test
+    public void shouldRetrieveReviewsForArticleNameTermInLowerCase() throws Exception {
+        Query query = new TermQuery(new Term(CommentedReviewIndexer.ARTICLE_NAME_FIELD, "lucene"));
+        TopDocs topDocs = searcher.search(query, QUERY_MATCHES_LIMIT);
+
+        assertThat(topDocs.totalHits, is(5L));
+    }
+}
