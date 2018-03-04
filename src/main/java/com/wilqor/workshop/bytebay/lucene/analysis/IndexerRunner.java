7f37e4024c729e91e35fbe3e5e7e431ed03d5fb5
wilqor <wilqor@gmail.com>
2018-03-04 14:15:38 +0100

    Introduce IndexerRunner

---
+++ src/main/java/com/wilqor/workshop/bytebay/lucene/analysis/IndexerRunner.java
@@ -0,0 +1,41 @@
+package com.wilqor.workshop.bytebay.lucene.analysis;
+
+import com.wilqor.workshop.bytebay.lucene.source.Source;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.function.Supplier;
+
+public class IndexerRunner<T> {
+    private static final Logger LOGGER = LogManager.getLogger(IndexerRunner.class);
+
+    private final Path indexRootDirectory;
+    private final Supplier<Indexer<T>> indexerSupplier;
+    private final Source<T> dataSource;
+
+    public IndexerRunner(Path indexRootDirectory, Supplier<Indexer<T>> indexerSupplier, Source<T> dataSource) {
+        this.indexRootDirectory = indexRootDirectory;
+        this.indexerSupplier = indexerSupplier;
+        this.dataSource = dataSource;
+    }
+
+    public void runIndexer() throws Exception {
+        if (Files.isDirectory(indexRootDirectory)) {
+            LOGGER.info("Index root directory \"{}\" exists, proceeding to clean up");
+            Files.walk(indexRootDirectory)
+                    .sorted(Comparator.reverseOrder())
+                    .map(Path::toFile)
+                    .forEach(File::delete);
+            LOGGER.info("Index root directory \"{}\" cleared");
+        }
+        try (Indexer<T> indexer = indexerSupplier.get()) {
+            LOGGER.info("Starting indexing...");
+            indexer.index(dataSource);
+            LOGGER.info("Indexing complete");
+        }
+    }
+}
---
+++ src/test/java/com/wilqor/workshop/bytebay/lucene/analysis/IndexerRunnerTest.java
@@ -0,0 +1,119 @@
+package com.wilqor.workshop.bytebay.lucene.analysis;
+
+import com.wilqor.workshop.bytebay.lucene.source.Source;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.KeywordAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.IOUtils;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class IndexerRunnerTest {
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    private IndexerRunner<DummyModel> indexerRunner;
+    private Path indexRootDirectory;
+
+    @Before
+    public void setUp() throws Exception {
+        indexRootDirectory = temporaryFolder.newFolder().toPath();
+        indexerRunner = new IndexerRunner<>(
+                indexRootDirectory,
+                () -> new DummyIndexer(indexRootDirectory),
+                () -> Stream.of(
+                        new DummyModel("1"),
+                        new DummyModel("2"),
+                        new DummyModel("3")
+                )
+        );
+    }
+
+    @Test
+    public void shouldKeepResultOfOneIndexingAfterMultipleRuns() throws Exception {
+        indexerRunner.runIndexer();
+        indexerRunner.runIndexer();
+        TopDocs topDocs = searchForAllDocumentsInIndex();
+        assertThat(topDocs.totalHits, is(3L));
+
+    }
+
+    private TopDocs searchForAllDocumentsInIndex() throws Exception {
+        Directory directory = FSDirectory.open(indexRootDirectory);
+        IndexReader reader = DirectoryReader.open(directory);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        TopDocs topDocs = searcher.search(new MatchAllDocsQuery(), 10);
+        IOUtils.close(reader, directory);
+        return topDocs;
+    }
+
+    private static final class DummyModel {
+        private final String id;
+
+        private DummyModel(String id) {
+            this.id = id;
+        }
+
+        public String getId() {
+            return id;
+        }
+    }
+
+    private static class DummyIndexer implements Indexer<DummyModel> {
+        private final Directory directory;
+
+        private DummyIndexer(Path indexRootDirectory) {
+            try {
+                directory = FSDirectory.open(indexRootDirectory);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public void index(Source<DummyModel> source) {
+            Analyzer analyzer = new KeywordAnalyzer();
+            IndexWriterConfig indexWriterConfig = new IndexWriterConfig(analyzer);
+            try (IndexWriter writer = new IndexWriter(directory, indexWriterConfig)) {
+                source.stream().forEach(
+                        dummyModel -> {
+                            Document document = new Document();
+                            document.add(new StringField("id", dummyModel.getId(), Field.Store.YES));
+                            try {
+                                writer.addDocument(document);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                );
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        @Override
+        public void close() throws Exception {
+            directory.close();
+        }
+    }
+}
