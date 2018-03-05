aed8088b473cc70e6854a62afdc4d5ef3c33f2a6
Zbyszko Papierski <zbyszko.papierski@gmail.com>
2018-03-05 20:09:58 +0100

    Added Angular app and task to install and run it to gradle

--- src/main/java/com/wilqor/workshop/bytebay/lucene/wikipediasearch/WikiediaSearchApp.java
+++ src/main/java/com/wilqor/workshop/bytebay/lucene/wikipediasearch/WikiediaSearchApp.java
@@ -1,7 +1,12 @@
 package com.wilqor.workshop.bytebay.lucene.wikipediasearch;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WikiediaSearchApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WikiediaSearchApp.class);
+    }
+
 }
