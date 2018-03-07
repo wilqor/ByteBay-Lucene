b7e05d040a215f73d84f4f99e0eeaff7ac2977f9
wilqor <wilqor@gmail.com>
2018-03-07 20:49:58 +0100

    Add flattened document reading to KeywordTokenizerExample

--- src/main/java/com/wilqor/workshop/bytebay/lucene/analysis/KeywordTokenizerExample.java
+++ src/main/java/com/wilqor/workshop/bytebay/lucene/analysis/KeywordTokenizerExample.java
@@ -12,8 +12,15 @@ import org.apache.lucene.analysis.core.KeywordTokenizer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.IOUtils;
 
@@ -52,7 +59,7 @@ public class KeywordTokenizerExample {
             });
         }
 
-        protected Document mapToDocument(SimpleReview sourceItem) {
+        Document mapToDocument(SimpleReview sourceItem) {
             Document reviewDocument = new Document();
             reviewDocument.add(new StringField(USER_NAME_FIELD, sourceItem.getUserName(), Field.Store.YES));
             reviewDocument.add(new StringField(THUMB_FIELD, sourceItem.getThumb().name(), Field.Store.YES));
@@ -76,10 +83,45 @@ public class KeywordTokenizerExample {
         }
     }
 
+    private static class SimpleReviewReader implements AutoCloseable {
+        private static final Logger LOGGER = LogManager.getLogger(SimpleReviewReader.class);
+        private static final int QUERY_MATCHES_LIMIT = 10;
+
+        private Directory directory;
+        private IndexReader reader;
+        private IndexSearcher searcher;
+
+        SimpleReviewReader(Path directoryPath) throws IOException {
+            directory = FSDirectory.open(directoryPath);
+            reader = DirectoryReader.open(directory);
+            searcher = new IndexSearcher(reader);
+        }
+
+        void read() throws IOException {
+            TopDocs topDocs = searcher.search(new MatchAllDocsQuery(), QUERY_MATCHES_LIMIT);
+            for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+                Document review = searcher.doc(scoreDoc.doc);
+                LOGGER.info("Found review - user name: {}, thumb: {}, article name: {}",
+                        review.get(SimpleReviewIndexer.USER_NAME_FIELD),
+                        review.get(SimpleReviewIndexer.THUMB_FIELD),
+                        review.get(SimpleReviewIndexer.ARTICLE_NAME_FIELD)
+                );
+            }
+        }
+
+        @Override
+        public void close() throws Exception {
+            IOUtils.close(reader, directory);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Path pathForIndex = ConfigLoader.LOADER.getPathForIndex(IndexType.KEYWORD_TOKENIZER_EXAMPLE);
         try (Indexer<SimpleReview> indexer = new SimpleReviewIndexer(pathForIndex, new KeywordTokenizingAnalyzer())) {
             indexer.index(Source.SIMPLE_MODEL);
+        }
+        try (SimpleReviewReader reader = new SimpleReviewReader(pathForIndex)) {
+            reader.read();
         }
     }
 }
