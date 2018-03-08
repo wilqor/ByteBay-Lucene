a3fb27407fdbeac986a64cc23ac426856a721a8a
wilqor <wilqor@gmail.com>
2018-03-08 13:24:56 +0100

    Add more useful examples to HTMLStripCharFilterExampleTest

--- src/test/java/com/wilqor/workshop/bytebay/lucene/analysis/HTMLStripCharFilterExampleTest.java
+++ src/test/java/com/wilqor/workshop/bytebay/lucene/analysis/HTMLStripCharFilterExampleTest.java
@@ -1,40 +1,82 @@
 package com.wilqor.workshop.bytebay.lucene.analysis;
 
-import com.wilqor.workshop.bytebay.lucene.BaseReadingTest;
-import com.wilqor.workshop.bytebay.lucene.config.ConfigLoader;
-import com.wilqor.workshop.bytebay.lucene.config.IndexType;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.nio.file.Path;
+
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.junit.Test;
 
-import java.nio.file.Path;
-
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import com.wilqor.workshop.bytebay.lucene.BaseReadingTest;
+import com.wilqor.workshop.bytebay.lucene.config.ConfigLoader;
+import com.wilqor.workshop.bytebay.lucene.config.IndexType;
 
 public class HTMLStripCharFilterExampleTest extends BaseReadingTest {
-    private static final int QUERY_MATCHES_LIMIT = 3;
+	private static final int QUERY_MATCHES_LIMIT = 3;
 
-    @Override
-    protected Path provideDirectoryPath() {
-        return ConfigLoader.LOADER.getPathForIndex(IndexType.HTML_CHAR_STRIP_FILTER_EXAMPLE);
-    }
+	@Override
+	protected Path provideDirectoryPath() {
+		return ConfigLoader.LOADER.getPathForIndex(IndexType.HTML_CHAR_STRIP_FILTER_EXAMPLE);
+	}
 
-    @Test
-    public void shouldRetrieveCommentsForTermInsideHTMLTag() throws Exception {
-        Query query = new TermQuery(new Term(WhitespaceTokenizerExample.CommentedReviewIndexer.COMMENT_FIELD, "polecam"));
-        TopDocs topDocs = searcher.search(query, QUERY_MATCHES_LIMIT);
+	@Test
+	public void shouldRetrieveCommentsForTermInsideHTMLTag() throws Exception {
+		Query query = new TermQuery(new Term(WhitespaceTokenizerExample.CommentedReviewIndexer.COMMENT_FIELD, "polecam"));
+		TopDocs topDocs = searcher.search(query, QUERY_MATCHES_LIMIT);
 
-        assertThat(topDocs.totalHits, is(2L));
-    }
+		assertThat(topDocs.totalHits, is(2L));
+	}
 
-    @Test
-    public void shouldRetrieveCommentsForTermInsideNestedHTMLTagWithAttribute() throws Exception {
-        Query query = new TermQuery(new Term(WhitespaceTokenizerExample.CommentedReviewIndexer.COMMENT_FIELD, "Serdecznie"));
-        TopDocs topDocs = searcher.search(query, QUERY_MATCHES_LIMIT);
+	@Test
+	public void shouldRetrieveCommentsForTermInsideNestedHTMLTagWithAttribute() throws Exception {
+		Query query = new TermQuery(new Term(WhitespaceTokenizerExample.CommentedReviewIndexer.COMMENT_FIELD, "Serdecznie"));
+		TopDocs topDocs = searcher.search(query, QUERY_MATCHES_LIMIT);
 
-        assertThat(topDocs.totalHits, is(1L));
-    }
+		assertThat(topDocs.totalHits, is(1L));
+	}
+
+	@Test
+	public void shouldRetrieveZeroCommentsForHTMLAttribute() throws Exception {
+		Query query = new TermQuery(new Term(WhitespaceTokenizerExample.CommentedReviewIndexer.COMMENT_FIELD, "highlighted"));
+		TopDocs topDocs = searcher.search(query, QUERY_MATCHES_LIMIT);
+
+		assertThat(topDocs.totalHits, is(0L));
+	}
+
+	@Test
+	public void shouldRetrieveZeroCommentsForHTMLTagName() throws Exception {
+		Query query = new TermQuery(new Term(WhitespaceTokenizerExample.CommentedReviewIndexer.COMMENT_FIELD, "strong"));
+		TopDocs topDocs = searcher.search(query, QUERY_MATCHES_LIMIT);
+
+		assertThat(topDocs.totalHits, is(0L));
+	}
+
+	@Test
+	public void shouldRetrieveZeroCommentsForHTMLOpeningTag() throws Exception {
+		Query query = new TermQuery(new Term(WhitespaceTokenizerExample.CommentedReviewIndexer.COMMENT_FIELD, "<p>"));
+		TopDocs topDocs = searcher.search(query, QUERY_MATCHES_LIMIT);
+
+		assertThat(topDocs.totalHits, is(0L));
+	}
+
+	@Test
+	public void shouldRetrieveZeroCommentsForHTMLClosingTag() throws Exception {
+		Query query = new TermQuery(new Term(WhitespaceTokenizerExample.CommentedReviewIndexer.COMMENT_FIELD, "</p>"));
+		TopDocs topDocs = searcher.search(query, QUERY_MATCHES_LIMIT);
+
+		assertThat(topDocs.totalHits, is(0L));
+	}
+
+	@Test
+	public void shouldRetrieveZeroCommentsForHTMLTagWithContent() throws Exception {
+		Query query = new TermQuery(
+				new Term(WhitespaceTokenizerExample.CommentedReviewIndexer.COMMENT_FIELD, "<strong>polecam</strong>"));
+		TopDocs topDocs = searcher.search(query, QUERY_MATCHES_LIMIT);
+
+		assertThat(topDocs.totalHits, is(0L));
+	}
 }
