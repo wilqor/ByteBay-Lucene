8f8c7aee0ba4dfa4638af8d6fe1f70349aa0c748
wilqor <wilqor@gmail.com>
2018-03-08 11:44:04 +0100

    Fix NPE and another exception during example Wikipedia indexing

--- src/main/java/com/wilqor/workshop/bytebay/lucene/source/Source.java
+++ src/main/java/com/wilqor/workshop/bytebay/lucene/source/Source.java
@@ -10,7 +10,7 @@ public interface Source<T> {
     Source<SimpleReview> SIMPLE_MODEL = new JsonSource<>("simple_model.json.batch", SimpleReview.class);
     Source<CommentedReview> COMMENTED_MODEL = new JsonSource<>("commented_model.json.batch", CommentedReview.class);
     Source<CommentedReview> COMMENTED_HTML_MODEL = new JsonSource<>("commented_model_html.json.batch", CommentedReview.class);
-    Source<WikipediaPage> WIKIPEDIA_PAGE_MODEL = new WikipediaSource("plwiki.json.batch");
+    Source<WikipediaPage> WIKIPEDIA_PAGE_MODEL = new WikipediaSource("plwiki_head.json.batch.bz2");
 
     Stream<T> stream();
 }
