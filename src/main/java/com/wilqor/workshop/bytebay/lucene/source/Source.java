ebcc9e597482cef2342373111dfd498a463edb65
wilqor <wilqor@gmail.com>
2018-03-14 22:29:59 +0100

    Fix path to Wikipedia resource file

--- src/main/java/com/wilqor/workshop/bytebay/lucene/source/Source.java
+++ src/main/java/com/wilqor/workshop/bytebay/lucene/source/Source.java
@@ -10,7 +10,7 @@ public interface Source<T> {
     Source<SimpleReview> SIMPLE_MODEL = new JsonSource<>("simple_model.json.batch", SimpleReview.class);
     Source<CommentedReview> COMMENTED_MODEL = new JsonSource<>("commented_model.json.batch", CommentedReview.class);
     // to use full Wikipedia pages archive, change name of file obtained from src/main/resources
-    Source<WikipediaPage> WIKIPEDIA_PAGE_MODEL = new WikipediaSource("plwik.json.batch.bz2");
+    Source<WikipediaPage> WIKIPEDIA_PAGE_MODEL = new WikipediaSource("plwiki_head.json.batch.bz2");
 
     Stream<T> stream();
 }
